fced71434ddc48f1ca1e63d6ec151bd0c4d4c4c2
Bianca <42931657+BiancaRapp@users.noreply.github.com>
2024-12-22 10:39:45 +0100

    feat: configure celery

--- src/flightsearch/settings.py
+++ src/flightsearch/settings.py
@@ -133,3 +133,8 @@ STATIC_ROOT = BASE_DIR / "static"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Celery settings
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
