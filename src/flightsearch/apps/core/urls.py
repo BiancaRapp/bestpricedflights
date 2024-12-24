df497be565dcfba49b40b295f82271d5debc7b6c
Bianca <42931657+BiancaRapp@users.noreply.github.com>
2024-12-24 09:20:23 +0100

    feat: add view for destination list

--- src/flightsearch/apps/core/urls.py
+++ src/flightsearch/apps/core/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from flightsearch.apps.core.views import search_flights
 
+from .views import DestinationListView
+
 urlpatterns = [
     path("search/<str:origin>/<str:travel_class>/<str:trip_type>", search_flights),
     path("search/<str:origin>/<str:travel_class>/", search_flights),
     path("search/<str:origin>/", search_flights),
+    path("list/destinations/<str:destination>", DestinationListView.as_view()),
+    path("list/destinations/", DestinationListView.as_view()),
 ]
--- src/flightsearch/apps/core/views.py
+++ src/flightsearch/apps/core/views.py
@@ -1,7 +1,12 @@
+import structlog
 from django.http import JsonResponse
+from django.views.generic import ListView
 
+from .models import Offer
 from .tasks import fetch_and_store_destinations_task
 from .utils import TravelClass, TripType, find_destinations
+
+logger = structlog.get_logger(__name__)
 
 
 def search_flights(request, origin, travel_class=TravelClass.BUSINESS, trip_type=TripType.RETURN):  # noqa: ARG001
@@ -10,3 +15,16 @@ def search_flights(request, origin, travel_class=TravelClass.BUSINESS, trip_type
 
     fetch_and_store_destinations_task.delay(origin_code=origin)
     return JsonResponse(response.json())
+
+
+class DestinationListView(ListView):
+    template_name = "destination_list.html"
+    model = Offer
+
+    def get_queryset(self):
+        offers = Offer.objects.all()
+        destination_code = self.kwargs.get("destination")
+        if destination_code:
+            logger.debug("Filter list for destination", destination=destination_code)
+            offers = offers.filter(trip__destination__code=destination_code)
+        return offers.select_related("trip")
