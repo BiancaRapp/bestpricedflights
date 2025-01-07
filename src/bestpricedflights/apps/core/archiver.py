cc3f220023fff18a2aaadef0201073e62fc3457a
Bianca <42931657+BiancaRapp@users.noreply.github.com>
2025-01-07 20:08:44 +0100

    refactor: remove date dependency between methods

--- src/bestpricedflights/apps/core/archiver.py
+++ src/bestpricedflights/apps/core/archiver.py
@@ -3,8 +3,9 @@ from django.db.models import Q
 from bestpricedflights.apps.core.models import Offer, Trip
 
 
-def archive_offers_of_unavailable_trips(origin_code, today, travel_class, trip_type):
+def archive_unavailable_offers(origin_code, travel_class, trip_type):
+    latest_fetched_on = Trip.objects.order_by("-fetched_on").first().fetched_on
     unavailable_trips = Trip.objects.exclude(
-        Q(fetched_on=today) | ~Q(origin__code=origin_code, travel_class=travel_class, trip_type=trip_type)
+        Q(fetched_on=latest_fetched_on) | ~Q(origin__code=origin_code, travel_class=travel_class, trip_type=trip_type)
     )
     Offer.objects.filter(trip__in=unavailable_trips).update(is_archived=True)
