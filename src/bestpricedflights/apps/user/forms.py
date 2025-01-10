71175cd81a0cdf8246b0a4494346eda9742c4ad6
Bianca <42931657+BiancaRapp@users.noreply.github.com>
2025-01-10 21:08:10 +0100

    feat: add option to sign up

--- src/bestpricedflights/apps/user/forms.py
+++ src/bestpricedflights/apps/user/forms.py
@@ -1,6 +1,24 @@
+from crispy_forms.bootstrap import FormActions
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
-from django.contrib.auth.forms import AuthenticationForm
+from crispy_forms.layout import HTML, Layout, Submit
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+from bestpricedflights.apps.core.models import City
+from bestpricedflights.apps.user.models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = (*UserCreationForm.Meta.fields, "preferred_origin_city")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["preferred_origin_city"].queryset = City.objects.filter(is_origin=True)
+
+        self.helper = FormHelper()
+        self.helper.add_input(Submit("submit", "Sign Up"))
 
 
 class CustomAuthenticationForm(AuthenticationForm):
@@ -8,4 +26,10 @@ class CustomAuthenticationForm(AuthenticationForm):
         super().__init__(*args, **kwargs)
 
         self.helper = FormHelper()
-        self.helper.add_input(Submit("login", "Login"))
+        self.helper.layout = Layout(
+            *self.fields,
+            FormActions(
+                Submit("login", "Log In"),
+            ),
+            HTML('<p>No account yet? <a href="{% url "signup" %}">Sign Up</a></p>'),
+        )
--- src/bestpricedflights/apps/user/urls.py
+++ src/bestpricedflights/apps/user/urls.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.views import LoginView
 from django.urls import include, path
 
+from bestpricedflights.apps.user import views
+
 from .forms import CustomAuthenticationForm
 
 urlpatterns = [
     path("login/", LoginView.as_view(form_class=CustomAuthenticationForm), name="login"),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
     path("", include("django.contrib.auth.urls")),
 ]
