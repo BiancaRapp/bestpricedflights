96edd29a6b8ffcc872cef7830d1d3dbc8d863702
Bianca <42931657+BiancaRapp@users.noreply.github.com>
2025-01-10 20:25:21 +0100

    fix: skip fetching destination if origin code is empty

--- src/bestpricedflights/apps/core/tasks.py
+++ src/bestpricedflights/apps/core/tasks.py
@@ -14,7 +14,9 @@ logger = structlog.get_logger(__name__)
 
 @app.task
 def collect_destinations_for_multiple_origins_task():
-    for origin_code in settings.ORIGIN_CODES:
+    origin_codes = settings.ORIGIN_CODES
+    logger.debug("Collecting destinations for multiple origins", origin_codes=origin_codes)
+    for origin_code in origin_codes:
         fetch_and_store_destinations_task(origin_code=origin_code, travel_class=TravelClass.BUSINESS)
 
 
@@ -22,11 +24,17 @@ def collect_destinations_for_multiple_origins_task():
 def fetch_and_store_destinations_task(
     origin_code: str, travel_class=TravelClass.BUSINESS.value, trip_type=TripType.RETURN.value
 ):
+    if not origin_code:
+        logger.error("No origin code provided", origin_code=origin_code, travel_class=travel_class, trip_type=trip_type)
+        return
+
     response = find_destinations(origin_code, travel_class, trip_type)
     if not response.ok:
         logger.error(
             "Failed fetching destinations",
-            extra={"origin_code": origin_code, "status_code": response.status_code, "text": response.text},
+            origin_code=origin_code,
+            status_code=response.status_code,
+            response_text=response.text,
         )
         return
 
