b85b04f3963a86e29f1981e1cf450e1b559a7adb
Bianca <42931657+BiancaRapp@users.noreply.github.com>
2025-01-12 15:47:27 +0100

    feat: add edit profile view

--- src/bestpricedflights/apps/user/views.py
+++ src/bestpricedflights/apps/user/views.py
@@ -1,10 +1,19 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
-from bestpricedflights.apps.user.forms import CustomUserCreationForm
+from bestpricedflights.apps.user.forms import CustomUserCreationForm, EditProfileForm
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+class EditProfileView(LoginRequiredMixin, UpdateView):
+    form_class = EditProfileForm
+    template_name = "profile/update.html"
+
+    def get_object(self, queryset=None):  # noqa: ARG002
+        return self.request.user
